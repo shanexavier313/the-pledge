7af79a9a1f66b1645919a151fdde85857239fc8e
webdeveloper001 <kd19850511@gmail.com>
2020-10-15 08:49:13 -0400

    feat: add redux
    fix: token issue

---
+++ frontend/src/redux/constants.js
@@ -0,0 +1,5 @@
+// Auth actions
+export const ACTION_LOGIN = "ACTION_LOGIN";
+export const ACTION_LOGIN_SUCCESS = "ACTION_LOGIN_SUCCESS";
+export const ACTION_LOGIN_FAIL = "ACTION_LOGIN_FAIL";
+export const ACTION_LOGOUT = "ACTION_LOGOUT";
