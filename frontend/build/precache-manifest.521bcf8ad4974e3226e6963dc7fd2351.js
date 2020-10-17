e9786732c5d5413ddac17d83dbf6a64abec0c796
webdeveloper001 <kd19850511@gmail.com>
2020-10-17 16:05:34 -0400

    build app

---
+++ frontend/build/precache-manifest.521bcf8ad4974e3226e6963dc7fd2351.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faf32a07cb7aa7d7d7d442668e215cf5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00667cfcc4450ba9fb0c",
+    "url": "/static/js/2.2014c281.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "/static/js/2.2014c281.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee79d3e1e8d431cb83b1",
+    "url": "/static/js/main.ab02efdf.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
