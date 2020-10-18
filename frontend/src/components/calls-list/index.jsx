fef4759ce6e943bfa405d6c53b85c2ed0720d8de
Shaan Savarirayan <shaan.savarirayan@gmail.com>
2020-10-17 22:09:46 -0400

    Reconfigure material ui imports and add babel plugins to minimize bundle size

--- frontend/src/components/calls-list/index.jsx
+++ frontend/src/components/calls-list/index.jsx
@@ -1,25 +1,31 @@
 import React from 'react'
-import Grid from '@material-ui/core/Grid'
-import Button from '@material-ui/core/Button'
+import { Button, Grid } from '@material-ui/core'
 import { navigate } from '@reach/router'
 import CallItem from '../call-item'
 const CallsList = ({ calls, recipients, updateCallNotesAction }) => {
-	return (
-		<>
-			{calls.map((call, index) => {
-				const recipient = recipients.find((item) => item.id === call.recipient)
-				return <CallItem call={call} recipient={recipient} key={index} updateCallNotesAction={updateCallNotesAction} />
-			})}
-			<Grid item xs={12}>
-				<Button
-					variant="contained"
-					color="primary"
-					onClick={() => navigate('create-call')}
-					fullWidth>
-					Add Call +
-				</Button>
-			</Grid>
-		</>
-	)
+  return (
+    <>
+      {calls.map((call, index) => {
+        const recipient = recipients.find((item) => item.id === call.recipient)
+        return (
+          <CallItem
+            call={call}
+            recipient={recipient}
+            key={index}
+            updateCallNotesAction={updateCallNotesAction}
+          />
+        )
+      })}
+      <Grid item xs={12}>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => navigate('create-call')}
+          fullWidth>
+          Add Call +
+        </Button>
+      </Grid>
+    </>
+  )
 }
 export default CallsList
