e17fb6cfbee8a4c6d2416478f6f2936fba12028e
Shaan Savarirayan <shaan.savarirayan@gmail.com>
2020-10-20 03:03:28 -0400

    Add bones of resources page

---
+++ frontend/src/components/resources/slides.js
@@ -0,0 +1,46 @@
+// Framework 1
+import Framework1_1 from 'static/images/framework1-1.png'
+import Framework1_2 from 'static/images/framework1-2.png'
+import Framework1_3 from 'static/images/framework1-3.png'
+import Framework1_4 from 'static/images/framework1-4.png'
+import Framework1_5 from 'static/images/framework1-5.png'
+import Framework1_6 from 'static/images/framework1-6.png'
+import Framework1_7 from 'static/images/framework1-7.png'
+// Framework 2
+import Framework2_1 from 'static/images/framework2-1.png'
+import Framework2_2 from 'static/images/framework2-2.png'
+import Framework2_3 from 'static/images/framework2-3.png'
+import Framework2_4 from 'static/images/framework2-4.png'
+import Framework2_5 from 'static/images/framework2-5.png'
+import Framework2_6 from 'static/images/framework2-6.png'
+import Framework2_7 from 'static/images/framework2-7.png'
+import Framework2_8 from 'static/images/framework2-8.png'
+import Framework2_9 from 'static/images/framework2-9.png'
+import Framework2_10 from 'static/images/framework2-10.png'
+import Framework2_11 from 'static/images/framework2-11.png'
+import Framework2_12 from 'static/images/framework2-12.png'
+
+export const framework1Slides = [
+  Framework1_1,
+  Framework1_2,
+  Framework1_3,
+  Framework1_4,
+  Framework1_5,
+  Framework1_6,
+  Framework1_7,
+]
+
+export const framework2Slides = [
+  Framework2_1,
+  Framework2_2,
+  Framework2_3,
+  Framework2_4,
+  Framework2_5,
+  Framework2_6,
+  Framework2_7,
+  Framework2_8,
+  Framework2_9,
+  Framework2_10,
+  Framework2_11,
+  Framework2_12,
+]
