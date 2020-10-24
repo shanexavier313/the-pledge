7f94cd71fb19d949dfe010ab568ac31305071526
Shaan Savarirayan <shaan.savarirayan@gmail.com>
2020-10-24 18:19:44 -0400

    Run production build

---
+++ frontend/build/precache-manifest.bd6f80c82233466789dc2578f9f12b35.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    revision: 'c27b4bb10059eea0e74fc71be541a72c',
+    url: '/index.html',
+  },
+  {
+    revision: 'f6977977d11e5b864734',
+    url: '/static/js/2.1c71d1bb.chunk.js',
+  },
+  {
+    revision: '556d72da1c19c1e88e2917fa6505d7f0',
+    url: '/static/js/2.1c71d1bb.chunk.js.LICENSE.txt',
+  },
+  {
+    revision: '40f874d9bb8eaa8aac62',
+    url: '/static/js/main.eca154c9.chunk.js',
+  },
+  {
+    revision: '2747b914e9a60db2276f',
+    url: '/static/js/runtime-main.c8a21426.js',
+  },
+])
--- frontend/build/service-worker.js
+++ frontend/build/service-worker.js
@@ -11,29 +11,31 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
-
 importScripts(
-  "/precache-manifest.a7f047f29dffc3c4e67609b0b3f23d6c.js"
-);
+  'https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js',
+)
+
+importScripts('/precache-manifest.bd6f80c82233466789dc2578f9f12b35.js')
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
+    self.skipWaiting()
   }
-});
+})
 
-workbox.core.clientsClaim();
+workbox.core.clientsClaim()
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
  * requests for URLs in the manifest.
  * See https://goo.gl/S9QRab
  */
-self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+self.__precacheManifest = [].concat(self.__precacheManifest || [])
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
-  
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
-});
+workbox.routing.registerNavigationRoute(
+  workbox.precaching.getCacheKeyForURL('/index.html'),
+  {
+    blacklist: [/^\/_/, /\/[^/?]+\.[^/]+$/],
+  },
+)
