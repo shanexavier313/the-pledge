e03b6ad8b7aef4e04b997719f7355f38fd0cf51c
Shaan Savarirayan <shaan.savarirayan@gmail.com>
2020-10-24 15:17:28 -0400

    Refactor auth actinons and reducers

--- frontend/src/redux/reducers/authReducer.js
+++ frontend/src/redux/reducers/authReducer.js
@@ -1,31 +1,68 @@
 import * as actionTypes from '../constants'
 const initialState = {
-  isLoggedIn: false,
-  user: false
+  access: localStorage.getItem('access'),
+  refresh: localStorage.getItem('refresh'),
+  isLoggedIn: null,
+  user: null,
 }
 
 const authReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case actionTypes.ACTION_LOGIN_SUCCESS:
-      const { user } = action;
+  const { type, payload } = action
+
+  switch (type) {
+    case actionTypes.AUTHENTICATED_SUCCESS:
       return {
         ...state,
         isLoggedIn: true,
-        user
       }
     case actionTypes.ACTION_USER_UPDATE_SUCCESS:
-      debugger;
-      const { data } = action;
+      return {
+        ...state,
+        user: {
+          email: payload.email,
+          first_name: payload.first_name,
+          last_name: payload.last_name,
+        },
+      }
+    case actionTypes.LOGIN_SUCCESS:
+      localStorage.setItem('access', payload.access)
       return {
         ...state,
         isLoggedIn: true,
-        user: { ...state.user, ...data }
+        access: payload.access,
+        refresh: payload.refresh,
       }
-    case actionTypes.ACTION_LOGOUT:
+    case actionTypes.USER_LOADED_SUCCESS:
+      return {
+        ...state,
+        user: payload,
+      }
+    case actionTypes.SIGNUP_SUCCESS:
       return {
         ...state,
         isLoggedIn: false,
-        user: false
+      }
+    case actionTypes.AUTHENTICATED_FAIL:
+      return {
+        ...state,
+        isLoggedIn: false,
+      }
+    case actionTypes.USER_LOADED_FAIL:
+      return {
+        ...state,
+        user: null,
+      }
+    case actionTypes.SIGNUP_FAIL:
+    case actionTypes.LOGIN_FAIL:
+    case actionTypes.LOGOUT:
+      localStorage.removeItem('access')
+      localStorage.removeItem('refresh')
+      return {
+        ...state,
+        access: null,
+        refresh: null,
+        isLoggedIn: false,
+        user: null,
       }
     default:
       return state
