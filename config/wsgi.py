57a2e5aa207cb5dbe5958ea2a5fcd6cff0d009a0
Shaan Savarirayan <shaan.savarirayan@gmail.com>
2020-10-25 12:45:45 -0400

    Reset wsgi and manage settings

--- config/wsgi.py
+++ config/wsgi.py
@@ -12,6 +12,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 
-os.environ.get("DJANGO_SETTINGS_MODULE", "config.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.dev")
 
 application = get_wsgi_application()
